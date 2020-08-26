3148ed07faef73068049f0fedf2426e199a61be7
asarfaty <asarfaty@vmware.com>
2020-08-26 08:47:41 +0200

    Remove leftover neutron-lbaas apis from all drivers
    Change-Id: I01e4421bf0a63efdbf0366b26a3e9e1cf3d1a917

--- vmware_nsx/services/lbaas/nsx_v/implementation/loadbalancer_mgr.py
+++ vmware_nsx/services/lbaas/nsx_v/implementation/loadbalancer_mgr.py
@@ -30,7 +30,6 @@ from vmware_nsx.plugins.nsx_v.vshield.common import (
     constants as vcns_const)
 from vmware_nsx.plugins.nsx_v.vshield.common import exceptions as nsxv_exc
 from vmware_nsx.services.lbaas import base_mgr
-from vmware_nsx.services.lbaas import lb_const
 from vmware_nsx.services.lbaas.nsx_v import lbaas_common as lb_common
 from vmware_nsx.services.lbaas.octavia import constants as oct_const
 
@@ -165,61 +164,6 @@ class EdgeLoadBalancerManagerFromDict(base_mgr.EdgeLoadbalancerBaseManager):
         #TODO(asarfaty): implement a better delete cascade for NSX-V
         self.delete(context, lb, completor)
 
-    def refresh(self, context, lb):
-        # TODO(kobis): implement
-        pass
-
-    def stats(self, context, lb):
-        binding = nsxv_db.get_nsxv_lbaas_loadbalancer_binding(context.session,
-                                                              lb['id'])
-
-        stats = _get_edge_loadbalancer_statistics(self.vcns,
-                                                  binding['edge_id'])
-
-        return stats
-
-    def get_operating_status(self, context, id, with_members=False):
-        """Return a map of the operating status of all connected LB objects """
-        lb_binding = nsxv_db.get_nsxv_lbaas_loadbalancer_binding(
-            context.session, id)
-        if not lb_binding or not lb_binding['edge_id']:
-            return {}
-        edge_id = lb_binding['edge_id']
-
-        lb_stats = self.vcns.get_loadbalancer_statistics(edge_id)
-        lb_status = (lb_const.ONLINE if lb_stats is not None
-                     else lb_const.OFFLINE)
-
-        statuses = {lb_const.LOADBALANCERS: [{'id': id, 'status': lb_status}],
-                    lb_const.LISTENERS: [],
-                    lb_const.POOLS: [],
-                    lb_const.MEMBERS: []}
-
-        for vs in lb_stats[1].get('virtualServer', []):
-            vs_id = vs['name'][4:]
-            vs_status = (lb_const.ONLINE if vs['status'] == 'OPEN'
-                         else lb_const.OFFLINE)
-            statuses[lb_const.LISTENERS].append(
-                {'id': vs_id, 'status': vs_status})
-
-        for pool in lb_stats[1].get('pool', []):
-            pool_id = pool['name'][5:]
-            pool_status = (lb_const.ONLINE if pool['status'] == 'UP'
-                           else lb_const.OFFLINE)
-            statuses[lb_const.POOLS].append(
-                {'id': pool_id, 'status': pool_status})
-            if with_members:
-                for member in pool.get('member', []):
-                    member_id = member['name'][7:]
-                    member_status = (lb_const.ONLINE
-                                     if member['status'] == 'UP'
-                                     else lb_const.OFFLINE)
-
-                    statuses[lb_const.MEMBERS].append(
-                        {'id': member_id, 'status': member_status})
-
-        return statuses
-
     def _handle_subnet_gw_change(self, *args, **kwargs):
         # As the Edge appliance doesn't use DHCP, we should change the
         # default gateway here when the subnet GW changes.
@@ -268,27 +212,3 @@ class EdgeLoadBalancerManagerFromDict(base_mgr.EdgeLoadbalancerBaseManager):
             if not found:
                 return False
         return True
-
-
-def _get_edge_loadbalancer_statistics(vcns, edge_id):
-    stats = {'bytes_in': 0,
-             'bytes_out': 0,
-             'active_connections': 0,
-             'total_connections': 0}
-
-    try:
-        lb_stats = vcns.get_loadbalancer_statistics(edge_id)
-
-    except nsxv_exc.VcnsApiException:
-        msg = (_('Failed to read load balancer statistics, edge: %s') %
-               edge_id)
-        raise n_exc.BadRequest(resource='edge-lbaas', msg=msg)
-
-    pools_stats = lb_stats[1].get('pool', [])
-    for pool_stats in pools_stats:
-        stats['bytes_in'] += pool_stats.get('bytesIn', 0)
-        stats['bytes_out'] += pool_stats.get('bytesOut', 0)
-        stats['active_connections'] += pool_stats.get('curSessions', 0)
-        stats['total_connections'] += pool_stats.get('totalSessions', 0)
-
-    return stats
