dc7c191303ef0f48afad6c6fd52844a9b335d04a
asarfaty <asarfaty@vmware.com>
2020-10-15 07:15:32 +0200

    Prepare the new W branch
    Change-Id: I076b563c42fd88495203c708f724f693df793c92

--- vmware_nsx/db/migration/alembic_migrations/versions/stein/expand/99bfcb6003c6_lbaas_error_no_member.py
+++ vmware_nsx/db/migration/alembic_migrations/versions/stein/expand/99bfcb6003c6_lbaas_error_no_member.py
@@ -28,7 +28,8 @@ revision = '99bfcb6003c6'
 down_revision = 'fc6308289aca'
 
 # milestone identifier, used by neutron-db-manage
-neutron_milestone = [migration.STEIN, migration.TRAIN, migration.USSURI]
+neutron_milestone = [migration.STEIN, migration.TRAIN, migration.USSURI,
+                     migration.VICTORIA]
 
 
 def upgrade():
